de629b20e6b2106e5d2024b45f34e444f6fd9d74
sultanul-kaisar <sultanulkaisar@gmail.com>
2022-11-14 16:05:18 +0600

    Email send complete

--- app/Mail/EmailDemo.php
+++ app/Mail/EmailDemo.php
@@ -35,5 +35,32 @@ class EmailDemo extends Mailable
      *
      * @return \Illuminate\Mail\Mailables\Envelope
      */
+    public function envelope()
+    {
+        return new Envelope(
+            subject: 'Email Demo',
+        );
+    }
 
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        return new Content(
+            markdown: 'Email.demoEmail',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [];
+    }
 }
